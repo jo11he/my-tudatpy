9ddc0b374fec9b33a3cb90b64ecb6c22e995e5a5
Space Jonas <jonas.hener@web.de>
2025-10-07 13:50:11 +0200

    expose ancillary settings as readonly property on ObservationSimulationSettings

--- src/tudatpy/estimation/observations_setup/observations_simulation_settings/expose_observations_simulation_settings.cpp
+++ src/tudatpy/estimation/observations_setup/observations_simulation_settings/expose_observations_simulation_settings.cpp
@@ -51,7 +51,15 @@ void expose_observations_simulation_settings( py::module& m )
                            R"doc(
          noise_function : Callable[ [float], numpy.ndarray[numpy.float64[m, 1]] ], default = None -
          Function providing the observation noise as a function of observation time (can be constant or time-dependent), default is None.
-      )doc" );
+      )doc" )
+        .def_property_readonly( "ancillary_settings",
+        &tss::ObservationSimulationSettings< TIME_TYPE >::getAncilliarySettings,,
+             R"doc(
+             Read-only property ObservationAncilliarySimulationSettings in ObservationSimulationSettings object.
+          )doc" );
+
+
+
     //            .def_property("observable_type",
     //                         &tss::ObservationSimulationSettings<double>::getObservableType,
     //                         &tss::ObservationSimulationSettings<double>::setObservableType,
